da09420d06a30548d5bd744bf5e1dbff466da937
Petter Holmström <petter.holmstrom@itmill.com>
2009-12-15 07:06:39 +0000

    New versions of PropertyMetadata
    git-svn-id: http://dev.vaadin.com/svn/addons/JPAContainer/trunk@10296 de684a11-831f-0410-ab27-cb86445eb682

--- src/main/java/com/vaadin/addons/jpacontainer/metadata/PropertyMetadata.java
+++ src/main/java/com/vaadin/addons/jpacontainer/metadata/PropertyMetadata.java
@@ -19,9 +19,10 @@ package com.vaadin.addons.jpacontainer.metadata;
 
 import java.io.Serializable;
 import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
 
 /**
- * This interface represents the metadata of a property. If the property
+ * This class represents the metadata of a property. If the property
  * is transient, this is an ordinary JavaBean property consisting of a getter method
  * and optionally a setter method. If the property is persistent, additional
  * information is provided by the {@link PersistentPropertyMetadata} interface.
@@ -30,24 +31,56 @@ import java.lang.annotation.Annotation;
  * @author Petter Holmström (IT Mill)
  * @since 1.0
  */
-public interface PropertyMetadata extends Serializable {
+public class PropertyMetadata implements Serializable {
+
+    private final String name;
+    private final Class<?> type;
+    final Method getter;
+    final Method setter;
+
+    /**
+     * Creates a new instance of <code>PropertyMetadata</code>. 
+     * 
+     * @param name the name of the property (must not be null).
+     * @param type the type of the property (must not be null).
+     * @param getter the getter method that can be used to read the value of the property (must not be null).
+     * @param setter the setter method that can be used to set the value of the property (must not be null).
+     */
+    PropertyMetadata(String name, Class<?> type, Method getter, Method setter) {
+        assert name != null : "name must not be null";
+        assert type != null : "type must not^ be null";
+        /*
+         * If we assert that getter and setter != null, PersistentPropertyMetadata
+         * will not work.
+         */
+        this.name = name;
+        this.type = type;
+        this.getter = getter;
+        this.setter = setter;
+    }
 
     /**
      * The name of the property.
      */
-    public String getName();
+    public String getName() {
+        return name;
+    }
 
     /**
      * The type of the property.
      */
-    public Class<?> getType();
+    public Class<?> getType() {
+        return type;
+    }
 
     /**
      * The annotations of the property, if any.
      *
      * @see #getAnnotation(java.lang.Class) 
      */
-    public Annotation[] getAnnotations();
+    public Annotation[] getAnnotations() {
+        return getter.getAnnotations();
+    }
 
     /**
      * Gets the annotation of the specified annotation class, if available.
@@ -57,14 +90,17 @@ public interface PropertyMetadata extends Serializable {
      * @param annotationClass the annotation class.
      * @return the annotation, or null if not found.
      */
-    public <T extends Annotation> T getAnnotation(Class<T> annotationClass);
+    public <T extends Annotation> T getAnnotation(Class<T> annotationClass) {
+        return getter.getAnnotation(annotationClass);
+    }
 
     /**
-     * Returns whether the property is writable or not. Persistent properties
-     * are always writable. Transient properties (i.e. JavaBean properties) are
+     * Returns whether the property is writable or not. Transient properties (i.e. JavaBean properties) are
      * only writable if they have a setter method.
      *
      * @return true if the property is writable, false if it is not.
      */
-    public boolean isWritable();
+    public boolean isWritable() {
+        return setter != null;
+    }
 }
