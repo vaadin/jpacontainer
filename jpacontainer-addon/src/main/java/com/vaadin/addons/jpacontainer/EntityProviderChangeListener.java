b6f0c8147e426338439a18c0ea61cad69184d649
Petter Holmström <petter.holmstrom@itmill.com>
2010-02-11 10:14:16 +0000

    Added interfaces for observing entity providers.
    git-svn-id: http://dev.vaadin.com/svn/addons/JPAContainer/trunk@11266 de684a11-831f-0410-ab27-cb86445eb682

---
+++ jpacontainer-addon/src/main/java/com/vaadin/addons/jpacontainer/EntityProviderChangeListener.java
@@ -0,0 +1,35 @@
+/*
+ * JPAContainer
+ * Copyright (C) 2010 Oy IT Mill Ltd
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.vaadin.addons.jpacontainer;
+
+/**
+ * Listener interface to be implemented by classes that want to be notified
+ * when a {@link EntityProvider} is changed.
+ *
+ * @see EntityProviderChangeNotifier
+ * @author Petter Holmström (IT Mill)
+ * @since 1.0
+ */
+public interface EntityProviderChangeListener<T> {
+
+    /**
+     * Notifies the client that <code>event</code> has occurred.
+     * @param event the occurred event (never null).
+     */
+    public void entityProviderChanged(EntityProviderChangeEvent<T> event);
+}
