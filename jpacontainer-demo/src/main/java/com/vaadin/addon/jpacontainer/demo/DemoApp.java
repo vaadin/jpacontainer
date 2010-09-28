471395b8bb310614d0d9c536b2a7dd2a9b3cece0
Henri Sara <henri.sara@itmill.com>
2010-09-28 12:26:32 +0000

    JPAContainer version number 1.0.4 to publish
    git-svn-id: http://dev.vaadin.com/svn/addons/JPAContainer/trunk@15247 de684a11-831f-0410-ab27-cb86445eb682

--- jpacontainer-demo/src/main/java/com/vaadin/addon/jpacontainer/demo/DemoApp.java
+++ jpacontainer-demo/src/main/java/com/vaadin/addon/jpacontainer/demo/DemoApp.java
@@ -76,6 +76,6 @@ public class DemoApp extends Application {
 
 	@Override
 	public String getVersion() {
-		return "1.0.3"; // TODO Replace this with a version number from the build system
+		return "1.0.4"; // TODO Replace this with a version number from the build system
 	}
 }
