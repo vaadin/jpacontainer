1278156f19f6e4ff96413de56004244c05668de5
Henri Sara <henri.sara@itmill.com>
2011-07-08 11:54:27 +0000

    Update JPAContainer licenses to CVALv2, configure Maven plugin that can update license headers in Java files on demand
    git-svn-id: http://dev.vaadin.com/svn/addons/JPAContainer/trunk@19678 de684a11-831f-0410-ab27-cb86445eb682

--- jpacontainer-demo/src/main/java/com/vaadin/addon/jpacontainer/demo/aspects/ProviderLoggerAspect.java
+++ jpacontainer-demo/src/main/java/com/vaadin/addon/jpacontainer/demo/aspects/ProviderLoggerAspect.java
@@ -1,19 +1,17 @@
 /*
  * JPAContainer
- * Copyright (C) 2010 Oy IT Mill Ltd
+ * Copyright (C) 2010-2011 Oy Vaadin Ltd
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * This program is available both under Commercial Vaadin Add-On
+ * License 2.0 (CVALv2) and under GNU Affero General Public License (version
+ * 3 or later) at your option.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
+ * See the file licensing.txt distributed with this software for more
+ * information about licensing.
  *
  * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * and CVALv2 along with this program.  If not, see
+ * <http://www.gnu.org/licenses/> and <http://vaadin.com/license/cval-2.0>.
  */
 package com.vaadin.addon.jpacontainer.demo.aspects;
 
