6d6aa8bed046cffff667fb47bfea44ff22030cfd
Henri Sara <henri.sara@itmill.com>
2011-07-11 11:33:34 +0000

    #7248 produce either AGPL or CVAL package of JPAContainer with correct Java file headers and license files
    git-svn-id: http://dev.vaadin.com/svn/addons/JPAContainer/trunk@19693 de684a11-831f-0410-ab27-cb86445eb682

--- jpacontainer-addon/src/test/java/com/vaadin/addon/jpacontainer/provider/emtests/AbstractCachingLocalEntityProviderEMTest.java
+++ jpacontainer-addon/src/test/java/com/vaadin/addon/jpacontainer/provider/emtests/AbstractCachingLocalEntityProviderEMTest.java
@@ -1,17 +1,5 @@
 /*
- * JPAContainer
- * Copyright (C) 2010-2011 Oy Vaadin Ltd
- *
- * This program is available both under Commercial Vaadin Add-On
- * License 2.0 (CVALv2) and under GNU Affero General Public License (version
- * 3 or later) at your option.
- *
- * See the file licensing.txt distributed with this software for more
- * information about licensing.
- *
- * You should have received a copy of the GNU Affero General Public License
- * and CVALv2 along with this program.  If not, see
- * <http://www.gnu.org/licenses/> and <http://vaadin.com/license/cval-2.0>.
+${license.header.text}
  */
 package com.vaadin.addon.jpacontainer.provider.emtests;
 
