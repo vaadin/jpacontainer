a4ca05c191141c261ea097b8eefc4e1fa8b50cf9
Matti Tahvonen <matti.tahvonen@itmill.com>
2011-11-01 11:21:07 +0000

    work in progress
    git-svn-id: http://dev.vaadin.com/svn/addons/JPAContainer/trunk@21847 de684a11-831f-0410-ab27-cb86445eb682

--- jpacontainer-addressbook-demo/src/main/java/com/vaadin/demo/jpaaddressbook/domain/Group.java
+++ jpacontainer-addressbook-demo/src/main/java/com/vaadin/demo/jpaaddressbook/domain/Group.java
@@ -1,8 +1,60 @@
 package com.vaadin.demo.jpaaddressbook.domain;
 
+import java.util.Set;
+
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Group {
 
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+	
+	private String name;
+	
+	@ManyToMany
+	private Set<Person> persons;
+	
+	@ManyToOne
+	private Group parent;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Set<Person> getPersons() {
+		return persons;
+	}
+
+	public void setPersons(Set<Person> persons) {
+		this.persons = persons;
+	}
+
+	public Group getParent() {
+		return parent;
+	}
+
+	public void setParent(Group parent) {
+		this.parent = parent;
+	}
+	
+	
 }
--- jpacontainer-addressbook-demo/src/main/java/com/vaadin/demo/jpaaddressbook/domain/Person.java
+++ jpacontainer-addressbook-demo/src/main/java/com/vaadin/demo/jpaaddressbook/domain/Person.java
@@ -1,8 +1,66 @@
 package com.vaadin.demo.jpaaddressbook.domain;
 
+import java.util.Set;
+
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class Person {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+	
+	private String firstName;
+	private String lastName;
+	private String street;
+	private String city;
+	private String zipCode;
+	private Set<Group> groups;
 
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getStreet() {
+		return street;
+	}
+	public void setStreet(String street) {
+		this.street = street;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getZipCode() {
+		return zipCode;
+	}
+	public void setZipCode(String zipCode) {
+		this.zipCode = zipCode;
+	}
+	public Set<Group> getGroups() {
+		return groups;
+	}
+	public void setGroups(Set<Group> groups) {
+		this.groups = groups;
+	}
 }
