0f073d63feade9eaf916dda41adc402622a1c082
Matti Tahvonen <matti.tahvonen@itmill.com>
2011-11-01 14:52:59 +0000

    toolbar + hooked search field to update filters in the person table
    git-svn-id: http://dev.vaadin.com/svn/addons/JPAContainer/trunk@21855 de684a11-831f-0410-ab27-cb86445eb682

--- jpacontainer-addressbook-demo/src/main/java/com/vaadin/demo/jpaaddressbook/AddressBookMainView.java
+++ jpacontainer-addressbook-demo/src/main/java/com/vaadin/demo/jpaaddressbook/AddressBookMainView.java
@@ -7,19 +7,26 @@ import javax.persistence.Persistence;
 import com.vaadin.addon.jpacontainer.EntityProvider;
 import com.vaadin.addon.jpacontainer.JPAContainer;
 import com.vaadin.addon.jpacontainer.filter.Filters;
+import com.vaadin.addon.jpacontainer.filter.Junction;
 import com.vaadin.addon.jpacontainer.filter.ValueFilter;
 import com.vaadin.addon.jpacontainer.provider.CachingMutableLocalEntityProvider;
 import com.vaadin.data.Property;
 import com.vaadin.data.Property.ValueChangeEvent;
 import com.vaadin.demo.jpaaddressbook.domain.Department;
 import com.vaadin.demo.jpaaddressbook.domain.Person;
+import com.vaadin.event.FieldEvents.TextChangeEvent;
+import com.vaadin.event.FieldEvents.TextChangeListener;
+import com.vaadin.ui.Alignment;
 import com.vaadin.ui.Button;
+import com.vaadin.ui.Component;
 import com.vaadin.ui.ComponentContainer;
+import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.HorizontalSplitPanel;
 import com.vaadin.ui.Table;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.Tree;
 import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.Button.ClickEvent;
 
 public class AddressBookMainView extends HorizontalSplitPanel implements
 		ComponentContainer {
@@ -37,6 +44,7 @@ public class AddressBookMainView extends HorizontalSplitPanel implements
 	private JPAContainer<Person> persons;
 	
 	private Department groupFilter;
+	private String textFilter;
 
 
 	static EntityManagerFactory emf = Persistence
@@ -75,7 +83,46 @@ public class AddressBookMainView extends HorizontalSplitPanel implements
 
 		personTable.setVisibleColumns(new Object[] { "firstName", "lastName", "department",
 				"phoneNumber", "street", "city", "zipCode" });
+
+		HorizontalLayout toolbar = new HorizontalLayout();
+		newButton = new Button("+");
+		newButton.addListener(new Button.ClickListener() {
+			
+			@Override
+			public void buttonClick(ClickEvent event) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+		
+		searchField = new TextField();
+		searchField.setInputPrompt("Search by name");
+		searchField.addListener(new TextChangeListener() {
+			
+
+			@Override
+			public void textChange(TextChangeEvent event) {
+				String text = event.getText();
+				textFilter = sanitizeInputText(text);
+				updateFilters();
+			}
+
+			private String sanitizeInputText(String text) {
+				if(text != null) {
+					return text.replaceAll("[^a-zA-Z0-9 ]", "");
+				}
+				return null;
+			}
+		});
+		
+		toolbar.addComponent(newButton);
+		toolbar.addComponent(searchField);
+		toolbar.setWidth("100%");
+		toolbar.setComponentAlignment(searchField, Alignment.TOP_RIGHT);
+		
+		verticalLayout.addComponent(toolbar );
 		verticalLayout.addComponent(personTable);
+		verticalLayout.setExpandRatio(personTable, 1);
 		verticalLayout.setSizeFull();
 		
 	}
@@ -125,6 +172,10 @@ public class AddressBookMainView extends HorizontalSplitPanel implements
 			} else {
 				persons.addFilter(Filters.eq("department", groupFilter));
 			}
+		}
+		if(textFilter != null && !textFilter.equals("")) {
+			Junction or = Filters.or(Filters.like("firstName", textFilter + "%", false),Filters.like("lastName", textFilter + "%", false));
+			persons.addFilter(or);
 		}
 		persons.applyFilters();
 	}
