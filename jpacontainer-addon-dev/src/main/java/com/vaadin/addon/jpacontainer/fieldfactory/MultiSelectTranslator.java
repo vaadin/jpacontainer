53514269f042b3cebead90d94f4806cdcf229b2f
Matti Tahvonen <matti.tahvonen@itmill.com>
2011-12-28 14:13:31 +0000

    Improved MultiSelectTranslator: now editing bi-directional relation from the "non-owning side" works.
    git-svn-id: http://dev.vaadin.com/svn/addons/JPAContainer/trunk@22488 de684a11-831f-0410-ab27-cb86445eb682

--- jpacontainer-addon-dev/src/main/java/com/vaadin/addon/jpacontainer/fieldfactory/MultiSelectTranslator.java
+++ jpacontainer-addon-dev/src/main/java/com/vaadin/addon/jpacontainer/fieldfactory/MultiSelectTranslator.java
@@ -6,7 +6,16 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import javax.persistence.ManyToMany;
+
 import com.vaadin.addon.jpacontainer.EntityContainer;
+import com.vaadin.addon.jpacontainer.EntityItem;
+import com.vaadin.addon.jpacontainer.EntityItemProperty;
+import com.vaadin.addon.jpacontainer.metadata.EntityClassMetadata;
+import com.vaadin.addon.jpacontainer.metadata.MetadataFactory;
+import com.vaadin.addon.jpacontainer.metadata.PropertyKind;
+import com.vaadin.addon.jpacontainer.metadata.PropertyMetadata;
+import com.vaadin.data.Property;
 import com.vaadin.ui.AbstractSelect;
 
 /**
@@ -17,6 +26,8 @@ import com.vaadin.ui.AbstractSelect;
 public class MultiSelectTranslator extends PropertyTranslator {
 
     private final AbstractSelect select;
+    private Boolean owningSide;
+    private String mappedBy;
 
     public MultiSelectTranslator(AbstractSelect select) {
         this.select = select;
@@ -46,7 +57,7 @@ public class MultiSelectTranslator extends PropertyTranslator {
     @SuppressWarnings({ "rawtypes", "unchecked" })
     @Override
     public Object translateToDatasource(Object formattedValue) throws Exception {
-
+        
         // NOTE, this currently works properly only if equals and hashcode
         // methods have been implemented correctly (both depending on identifier
         // of the entity)
@@ -61,7 +72,8 @@ public class MultiSelectTranslator extends PropertyTranslator {
         Collection value = (Collection) getPropertyDataSource().getValue();
 
         if (value == null) {
-            value = createNewCollectionForType(getPropertyDataSource().getType());
+            value = createNewCollectionForType(getPropertyDataSource()
+                    .getType());
         }
 
         HashSet orphaned = new HashSet(value);
@@ -72,27 +84,98 @@ public class MultiSelectTranslator extends PropertyTranslator {
             Object entity = getContainer().getEntityProvider().getEntity(id);
             if (!value.contains(entity)) {
                 value.add(entity);
+                addBackReference(entity);
             }
             orphaned.remove(entity);
         }
 
         // remove orphanded
-        for (Object object : orphaned) {
-            value.remove(object);
+        for (Object entity : orphaned) {
+            value.remove(entity);
+            removeBackReference(entity);
         }
 
+        if (!isOwningSide()) {
+            // refresh the item as modifying back references may also have
+            // changed the collections, without this we'd get concurrent
+            // modification exception.
+            getPropertyDataSource().getItem().refresh();
+        }
         return value;
+    }
+
+    @SuppressWarnings({ "rawtypes" })
+    private void removeBackReference(Object entity) {
+        if (!isOwningSide()) {
+            EntityItemProperty itemProperty = getBackReferenceItemProperty(entity);
+            Collection c = (Collection) itemProperty.getValue();
+            c.remove(getPropertyDataSource().getItem().getEntity());
+            itemProperty.setValue(c);
+        }
+    }
+
+    @SuppressWarnings({ "rawtypes", "unchecked" })
+    private EntityItemProperty getBackReferenceItemProperty(Object entity) {
+        EntityItem item = getContainer().getItem(
+                getContainer().getEntityProvider().getIdentifier(entity));
+        EntityItemProperty itemProperty = item.getItemProperty(mappedBy);
+        return itemProperty;
+    }
+
+    @SuppressWarnings({ "unchecked", "rawtypes" })
+    private void addBackReference(Object entity) {
+        if (!isOwningSide()) {
+            EntityItemProperty itemProperty = getBackReferenceItemProperty(entity);
+            Collection c = (Collection) itemProperty.getValue();
+            c.add(
+                    getPropertyDataSource().getItem().getEntity());
+            itemProperty.setValue(c);
+        }
+    }
+
+    /**
+     * Checks if the manytomany relation is owned by this side of the property.
+     * As a side effect detects the name of the owner property if the relation
+     * is owned by the other side.
+     * 
+     * @return false if bidirectional connection and the mapping has a mappedBy
+     *         parameter.
+     */
+    private boolean isOwningSide() {
+        if (owningSide == null) {
+            Class<?> entityClass = getPropertyDataSource().getItem()
+                    .getContainer().getEntityClass();
+            EntityClassMetadata<?> entityClassMetadata = MetadataFactory
+                    .getInstance().getEntityClassMetadata(entityClass);
+            PropertyMetadata property = entityClassMetadata
+                    .getProperty(getPropertyDataSource().getPropertyId());
+            ManyToMany annotation = property.getAnnotation(ManyToMany.class);
+            if (annotation.mappedBy() != null
+                    && !annotation.mappedBy().isEmpty()) {
+                owningSide = Boolean.FALSE;
+                mappedBy = annotation.mappedBy();
+                return owningSide;
+            }
+            owningSide = Boolean.TRUE;
+        }
+        return owningSide;
+    }
+
+    @Override
+    public EntityItemProperty getPropertyDataSource() {
+        return (EntityItemProperty) super.getPropertyDataSource();
     }
 
     static Collection createNewCollectionForType(Class<?> type)
             throws InstantiationException, IllegalAccessException {
         if (type.isInterface()) {
-            if(type == Set.class) {
+            if (type == Set.class) {
                 return new HashSet();
             } else if (type == List.class) {
                 return new ArrayList();
             } else {
-                throw new RuntimeException("Couldn't instantiate a collection for property.");
+                throw new RuntimeException(
+                        "Couldn't instantiate a collection for property.");
             }
         } else {
             return (Collection) type.newInstance();
